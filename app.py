57bd696c6c6317adeedee62c153f2e04a8438772
frankwong2001 <60854200+frankwong2001@users.noreply.github.com>
2023-07-27 17:28:26 +0800

    app.py

--- app.py
+++ app.py
@@ -5,11 +5,14 @@ import pandas as pd
 table1_df = pd.read_csv('Table_Input.csv', index_col=0)
 
 # Calculate values for Table 2
+alpha_value = table1_df.loc['A5'] + table1_df.loc['A20']
+beta_value = table1_df.loc['A15'] / table1_df.loc['A7']
+charlie_value = table1_df.loc['A13'] * table1_df.loc['A12']
+
+# Create DataFrame for Table 2
 table2_df = pd.DataFrame({
     'Category': ['Alpha', 'Beta', 'Charlie'],
-    'Value': [table1_df.loc['A5'] + table1_df.loc['A20'],
-              table1_df.loc['A15'] / table1_df.loc['A7'],
-              table1_df.loc['A13'] * table1_df.loc['A12']]
+    'Value': [alpha_value.item(), beta_value.item(), charlie_value.item()]
 })
 
 # Create the Streamlit app
@@ -18,5 +21,3 @@ st.dataframe(table1_df)
 
 st.title('Table 2')
 st.dataframe(table2_df)
-
-# To run the app: streamlit run app.py
