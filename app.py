cfc1daece36562984d5187f85cbd7db596367b99
frankwong2001 <60854200+frankwong2001@users.noreply.github.com>
2023-07-27 17:24:35 +0800

    app.py

--- app.py
+++ app.py
@@ -1,26 +1,22 @@
-from flask import Flask, render_template
+import streamlit as st
 import pandas as pd
 
-app = Flask(__name__)
+# Read Table 1 from CSV file
+table1_df = pd.read_csv('Table_Input.csv', index_col=0)
 
-# Read Table 1 data from CSV file
-table1_data = pd.read_csv('Table_Imput.csv', index_col=0)
+# Calculate values for Table 2
+table2_df = pd.DataFrame({
+    'Category': ['Alpha', 'Beta', 'Charlie'],
+    'Value': [table1_df.loc['A5'] + table1_df.loc['A20'],
+              table1_df.loc['A15'] / table1_df.loc['A7'],
+              table1_df.loc['A13'] * table1_df.loc['A12']]
+})
 
-@app.route('/')
-def display_tables():
-    # Calculate values for Table 2
-    alpha_value = table1_data.loc['A5', 'Value'] + table1_data.loc['A20', 'Value']
-    beta_value = table1_data.loc['A15', 'Value'] / table1_data.loc['A7', 'Value']
-    charlie_value = table1_data.loc['A13', 'Value'] * table1_data.loc['A12', 'Value']
+# Create the Streamlit app
+st.title('Table 1')
+st.dataframe(table1_df)
 
-    # Prepare Table 2 data
-    table2_data = [
-        {"Category": "Alpha", "Value": alpha_value},
-        {"Category": "Beta", "Value": beta_value},
-        {"Category": "Charlie", "Value": charlie_value}
-    ]
+st.title('Table 2')
+st.dataframe(table2_df)
 
-    return render_template('tables.html', table1=table1_data, table2=table2_data)
-
-if __name__ == '__main__':
-    app.run()
+# To run the app: streamlit run app.py
